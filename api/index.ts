d77301002fb1c0afc0a22eb0d8aa5ed1c5c13f98
tpluscode <tpluscode@users.noreply.github.com>
2019-07-19 22:02:45 +0200

    feat: entrypoint resource

---
+++ api/index.ts
@@ -0,0 +1,46 @@
+import * as cors from 'cors'
+import * as path from 'path'
+import * as hydraBox from 'hydra-box'
+import * as url from 'url'
+import * as express from 'express'
+import * as dotenv from 'dotenv'
+import * as dotenvExpand from 'dotenv-expand'
+
+dotenvExpand(dotenv.config())
+
+function logger (req, res, next) {
+  process.stdout.write(`${req.method} ${req.url} `)
+
+  res.on('finish', () => {
+    process.stdout.write(`${res.statusCode}\n`)
+  })
+
+  next()
+}
+
+function hydraMiddleware () {
+  return hydraBox.fromUrl('/api', 'file://' + path.join(__dirname, 'hydra/api.ttl'), {
+    debug: true,
+    sparqlEndpointUrl: process.env.SPARQL_ENDPOINT,
+    contextHeader: '/context/',
+  })
+}
+
+Promise.resolve().then(async () => {
+  const baseUrl = `${process.env.BASE_URI}`
+
+  const app = express()
+
+  app.use(logger)
+  app.use(cors({
+    exposedHeaders: ['link', 'location'],
+  }))
+  app.use(await hydraMiddleware())
+
+  const server = app.listen(url.parse(baseUrl).port, () => {
+    const host = server.address().address
+    const port = server.address().port
+
+    console.log(`listening at ${baseUrl}`)
+  })
+}).catch(err => console.error(err))
