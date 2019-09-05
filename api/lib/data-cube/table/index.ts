914e01c11b1f7ad68ae349f4d5b77df8a09a01c7
tpluscode <tpluscode@users.noreply.github.com>
2019-09-05 16:28:58 +0200

    feat: linking project to its fact table resource

---
+++ api/lib/data-cube/table/index.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction } from 'express'
+
+export function createFactTable (req: Request, res: Response, next: NextFunction) {
+  next()
+}
+
+export function get (req: Request, res: Response, next: NextFunction) {
+  next()
+}
