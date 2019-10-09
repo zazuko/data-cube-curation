f5e94008d9e00ac3e942fcd20dc8f7068dd3a0d6
tpluscode <tpluscode@users.noreply.github.com>
2019-10-09 11:17:26 +0200

    refactor: test runner rewritten in node to allow entry URL parameter

---
+++ api/test/run.ts
@@ -0,0 +1,34 @@
+import { spawn } from 'child_process'
+
+const scenarios = {
+  Entrypoint: '',
+  CreateDataCubeProject: '',
+  CreateProjectFactTable: 'project/fact-table-test',
+  CreateFactTableAttribute: 'project/add-attribute-test',
+}
+
+const promise = Object.entries(scenarios).reduce((promise, [ scenario, path ]) => {
+  return promise.then(() => {
+    return new Promise(async (resolve, reject) => {
+      const command = `hydra-validator e2e --docs test/${scenario}.hydra.json ${process.env.BASE_URI}${path}`
+      console.log(`\n------\n   ${command}\n------\n`)
+
+      const childProcess = spawn(
+        `hydra-validator`,
+        [`e2e`, `--docs`, `test/${scenario}.hydra.json`, `${process.env.BASE_URI}${path}`],
+        { stdio: 'inherit' })
+
+      childProcess.on('exit', code => {
+        if (code === 0) {
+          resolve()
+        }
+
+        reject(new Error('Last scenario failed. Stopping'))
+      })
+    })
+  })
+}, Promise.resolve())
+
+promise
+  .then(() => console.log('done'))
+  .catch(e => console.error(e))
