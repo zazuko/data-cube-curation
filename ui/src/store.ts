4781a0b7a59d2c7e30a1c5c02c3f83c0e0003515
Martin Maillard <self@martin-maillard.com>
2019-10-14 14:21:30 +0200

    feat(ui): add page that lists projects

--- ui/src/store.ts
+++ ui/src/store.ts
@@ -1,16 +1,34 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { Hydra } from 'alcaeus';
+import { ICollection } from 'alcaeus/types/Resources';
+import { Project } from './types';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    projects: [],
   },
+
   mutations: {
-
+    setProjects(state, projects) {
+      state.projects = projects;
+    },
   },
-  actions: {
 
+  actions: {
+    async getAllProjects({ commit }) {
+      const response = await Hydra.loadResource('http://localhost:5678/projects');
+      const projectsCollection = response.root as ICollection | null;
+
+      if (!projectsCollection) { return; }
+
+      const projects = projectsCollection.members.map((project) => ({
+          id: project.id,
+          name: project.get('http://schema.org/name') || '',
+      }));
+      commit('setProjects', projects);
+    },
   },
 });
---
+++ ui/src/views/Projects.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="projects-page">
+        <h2 class="title">My projects</h2>
+
+        <div class="actions">
+            <button class="button">New project</button>
+        </div>
+
+        <b-table :data="projects" :columns="columns" v-if="projects.length > 0">
+        </b-table>
+        <p v-if="projects.length === 0">You don't have any projects yet.</p>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import { Project } from '../types';
+
+
+@Component
+export default class Projects extends Vue {
+    columns = [
+        {label: 'Name', field: 'name'},
+        {label: 'ID', field: 'id'},
+        {label: 'Actions', field: ''},
+    ];
+
+    get projects(): Project[] {
+        return this.$store.state.projects;
+    }
+
+    created() {
+        this.$store.dispatch('getAllProjects');
+    }
+}
+</script>
