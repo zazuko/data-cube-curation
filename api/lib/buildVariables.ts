c28d63dbbc511bed813b5b42726540075d18c28f
tpluscode <tpluscode@users.noreply.github.com>
2019-11-05 09:55:25 +0100

    refactor(build-variables): separate getter of single and multiple values

--- api/lib/buildVariables.ts
+++ api/lib/buildVariables.ts
@@ -1,41 +1,50 @@
 import express from 'express'
 import { Term } from 'rdf-js'
 
+interface ReadOnlyVariable {
+  value: any | null;
+  values: any[];
+}
+
 class Variable {
-  private readonly __term: Term
+  private readonly __terms: Term[] = []
 
   public constructor (term?: Term) {
-    this.__term = term
+    if (term) {
+      this.__terms = [ term ]
+    }
   }
 
   public get value (): any | null {
-    if (this.__term) {
-      return this.__term.value
+    if (this.__terms.length === 1) {
+      return this.__terms[0].value
     }
 
     return null
   }
+
+  public get values (): any[] {
+    return this.__terms.map(term => term.value)
+  }
+
+  public push (term: Term) {
+    this.__terms.push(term)
+  }
 }
 
-export function buildVariables<T extends Record<string, string>> (req: express.DataCubeRequest, mappings: T): Record<keyof T, Variable> {
+export function buildVariables<T extends Record<string, string>> (req: express.DataCubeRequest, mappings: T): Record<keyof T, ReadOnlyVariable> {
   return Object.entries(mappings).reduce((locals, mapping) => {
     const name = mapping[0] as keyof T
     const property = mapping[1]
 
-    locals[name] = undefined
-
     req.graph.match(null, property).toArray().map(t => t.object).forEach((term) => {
       if (locals[name]) {
-        if (!Array.isArray(locals[name])) {
-          locals[name] = [locals[name]]
-        }
-
-        locals[name].push(new Variable(term))
+        locals[name].push(term)
       } else {
         locals[name] = new Variable(term)
       }
     })
 
     return locals
-  }, {} as any)
+  }, {} as any as Record<keyof T, Variable>)
 }
