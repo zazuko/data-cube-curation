a16fcfb74826e2edb0cc5d46c4d4cc32ef2d6c69
Martin Maillard <self@martin-maillard.com>
2019-11-12 16:57:12 +0100

    fix(ui): only show Source "delete" button if operation exists

--- ui/src/api/resources/source.ts
+++ ui/src/api/resources/source.ts
@@ -1,9 +1,15 @@
 import { HydraResource } from 'alcaeus/types/Resources'
-import { Constructor } from '../common'
+import { Constructor, findOperation } from '../common'
 import * as URI from '../uris'
 
 export function Mixin<B extends Constructor> (Base: B) {
   return class extends Base {
+    get actions () {
+      return {
+        delete: findOperation(this, URI.OP_SOURCE_DELETE)
+      }
+    }
+
     get name () {
       return this.get(URI.PROP_NAME)
     }
