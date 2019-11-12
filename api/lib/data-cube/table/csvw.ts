bb8f1e092bed3ccfa0bed235bf2f9270e2dc0e02
tpluscode <tpluscode@users.noreply.github.com>
2019-11-12 12:26:30 +0100

    refactor: wrap all async express handlers
    will nicely catch all those potential unhandled promise rejections and
    also reduces the need to explicitly declare most parameter types

--- api/lib/data-cube/table/csvw.ts
+++ api/lib/data-cube/table/csvw.ts
@@ -1,11 +1,12 @@
 import express from 'express'
+import asyncMiddleware from 'middleware-async'
 import { getTableId } from './index'
 import { getTableAndSource } from '../../read-graphs/table/csvw'
 import { buildCsvw } from '../../services/csvwBuilder'
 
-export async function get (req: express.DataCubeRequest, res: express.DataCubeResponse) {
+export const get = asyncMiddleware(async (req, res: express.DataCubeResponse) => {
   const tableId = getTableId(req)
   const tableDataset = await getTableAndSource(tableId)
 
   res.graph(await buildCsvw(tableDataset, tableId))
-}
+})
