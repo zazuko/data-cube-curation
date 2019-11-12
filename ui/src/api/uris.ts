7215d7cd4a441f1dfce6eb7766c10c45e452ef23
Martin Maillard <self@martin-maillard.com>
2019-11-12 14:27:35 +0100

    refactor(ui): split api client in multiple modules

---
+++ ui/src/api/uris.ts
@@ -0,0 +1,28 @@
+import { expand, prefixes } from '@zazuko/rdf-vocabularies'
+
+prefixes.dataCube = 'https://rdf-cube-curation.described.at/'
+
+export const TYPE_PROJECT = expand('dataCube:Project')
+export const TYPE_SOURCE = expand('dataCube:Source')
+export const TYPE_FACT_TABLE = expand('dataCube:FactTable')
+export const TYPE_DIMENSION_TABLE = expand('dataCube:DimensionTable')
+export const TYPE_TABLE = expand('dataCube:Table')
+
+export const PROP_NAME = expand('schema:name')
+export const PROP_SOURCE = expand('dataCube:source')
+export const PROP_IDENTIFIER_TEMPLATE = expand('dataCube:identifierTemplate')
+
+export const API_PROJECTS = expand('dataCube:api/projects')
+export const API_SOURCES = expand('dataCube:api/sources')
+export const API_TABLES = expand('dataCube:api/tables')
+export const OP_PROJECTS_GET = expand('dataCube:api/GetDataCubeProjects')
+export const OP_PROJECTS_CREATE = expand('dataCube:api/CreateProject')
+export const OP_PROJECT_DELETE = expand('dataCube:api/DeleteProject')
+export const OP_PROJECT_EDIT = expand('dataCube:api/ReplaceProject')
+export const OP_SOURCES_CREATE = expand('dataCube:api/AddSource')
+export const OP_TABLES_GET = expand('dataCube:api/GetTables')
+export const OP_TABLES_CREATE_DIMENSION = expand('dataCube:api/CreateDimensionTable')
+export const OP_TABLES_CREATE_FACT = expand('dataCube:api/PostFactTable')
+export const OP_TABLE_GET = expand('dataCube:api/GetTable')
+export const OP_TABLE_EDIT = expand('dataCube:api/EditTable')
+export const OP_TABLE_DELETE = expand('dataCube:api/DeleteTable')
