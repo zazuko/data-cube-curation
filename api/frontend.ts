0864ed019950d23d735d44ca4a4b554db47cea45
tpluscode <tpluscode@users.noreply.github.com>
2019-11-13 11:28:35 +0100

    fix(docker): use a simple history fallback middleware
    fixes #80

--- api/frontend.ts
+++ api/frontend.ts
@@ -1,13 +1,10 @@
-import history from 'connect-history-api-fallback'
+import fallback from 'express-history-api-fallback'
 import express from 'express'
 
 const app = express()
 
 const staticFileMiddleware = express.static('ui')
 app.use(staticFileMiddleware)
-app.use(history({
-  disableDotRule: true,
-  verbose: true,
-}))
+app.use(fallback('index.html', { root: `${__dirname}/ui` }))
 
 export default app
