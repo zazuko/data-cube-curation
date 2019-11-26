c0d1ccb54fdd80e4c6dce5fef16dc752f6cbfb8a
tpluscode <tpluscode@users.noreply.github.com>
2019-11-26 12:03:49 +0100

    fix: set auth headers to sparql endpoint

--- api/lib/storage/repository/create.ts
+++ api/lib/storage/repository/create.ts
@@ -3,13 +3,19 @@ import { SparqlGraphRepository } from '@tpluscode/fun-ddr-sparql-graph-repositor
 import SparqlHttp from 'sparql-http-client'
 import { prefixes, expand } from '@zazuko/rdf-vocabularies'
 import rdfFetch from 'hydra-box/lib/rdfFetch'
+import authHeader from '../../sparql/authentication'
 
 const base = process.env.BASE_URI
+const defaultHeaders: HeadersInit = {}
+if (authHeader) {
+  defaultHeaders.Authorization = authHeader
+}
 const sparqlClient = new SparqlHttp({
   endpointUrl: process.env.SPARQL_ENDPOINT,
   updateUrl: process.env.SPARQL_UPDATE_ENDPOINT || process.env.SPARQL_ENDPOINT,
   fetch: rdfFetch,
-})
+  defaultHeaders,
+} as any)
 
 const context = {
   '@vocab': 'https://rdf-cube-curation.described.at/',
