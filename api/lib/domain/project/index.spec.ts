6e674f7b6f74b901c7ff608ac75def80f81bc569
tpluscode <tpluscode@users.noreply.github.com>
2020-01-07 12:10:10 +0100

    fix(api): base URI was missing for projects

--- api/lib/domain/project/index.spec.ts
+++ api/lib/domain/project/index.spec.ts
@@ -1,4 +1,5 @@
-import { renameProject } from './index'
+import { updateProject } from './index'
+import { DomainError } from '@tpluscode/fun-ddr'
 
 describe('Project', () => {
   const nullProject = {
@@ -15,18 +16,20 @@ describe('Project', () => {
   })
 
   describe('rename command', () => {
-    it('does not emit event when new name is same as current', async () => {
+    it('does not emit event when nothing changes', async () => {
       // given
       const project = {
         ...nullProject,
         name: 'a name',
+        baseUri: 'urn:foo:',
       }
       const cmd = {
         newName: 'a name',
+        baseUri: 'urn:foo:',
       }
 
       // when
-      await renameProject(project, cmd, emitter)
+      await updateProject(project, cmd, emitter)
 
       // then
       expect(emitter.emit).not.toHaveBeenCalled()
@@ -37,17 +40,40 @@ describe('Project', () => {
       const project = {
         ...nullProject,
         name: 'foo',
+        baseUri: 'urn:foo:',
       }
       const cmd = {
         newName: 'bar',
+        baseUri: 'urn:foo:',
       }
 
       // when
-      await renameProject(project, cmd, emitter)
+      await updateProject(project, cmd, emitter)
 
       // then
       expect(emitter.emit).toHaveBeenCalledWith('ProjectRenamed', {
         name: 'bar',
+      })
+    })
+
+    it('emits an event with new base uri', async () => {
+      // given
+      const project = {
+        ...nullProject,
+        name: 'foo',
+        baseUri: 'urn:foo:',
+      }
+      const cmd = {
+        newName: 'foo',
+        baseUri: 'urn:bar:',
+      }
+
+      // when
+      await updateProject(project, cmd, emitter)
+
+      // then
+      expect(emitter.emit).toHaveBeenCalledWith('ProjectRebased', {
+        baseUri: 'urn:bar:',
       })
     })
 
@@ -56,16 +82,67 @@ describe('Project', () => {
       const project = {
         ...nullProject,
         name: 'foo',
+        baseUri: 'urn:foo:',
       }
       const cmd = {
         newName: 'bar',
+        baseUri: 'urn:foo:',
       }
 
       // when
-      const newState = await renameProject(project, cmd, emitter)
+      const newState = await updateProject(project, cmd, emitter)
 
       // then
       expect(newState.name).toBe('bar')
+    })
+
+    it('sets the baseUri property', async () => {
+      // given
+      const project = {
+        ...nullProject,
+        name: 'foo',
+        baseUri: 'urn:foo:',
+      }
+      const cmd = {
+        newName: 'foo',
+        baseUri: 'urn:bar:',
+      }
+
+      // when
+      const newState = await updateProject(project, cmd, emitter)
+
+      // then
+      expect(newState.baseUri).toBe('urn:bar:')
+    })
+
+    it('fails if name is missing', async () => {
+      // given
+      const project = {
+        ...nullProject,
+        name: 'foo',
+        baseUri: 'urn:foo:',
+      }
+      const cmd = {
+        baseUri: 'urn:foo:',
+      }
+
+      // then
+      await expect(updateProject(project, cmd as any, emitter)).rejects.toBeInstanceOf(DomainError)
+    })
+
+    it('fails if baseUri is missing', async () => {
+      // given
+      const project = {
+        ...nullProject,
+        name: 'foo',
+        baseUri: 'urn:foo:',
+      }
+      const cmd = {
+        newName: 'foo',
+      }
+
+      // then
+      await expect(updateProject(project, cmd as any, emitter)).rejects.toBeInstanceOf(DomainError)
     })
   })
 })
--- api/lib/domain/project/index.ts
+++ api/lib/domain/project/index.ts
@@ -1,4 +1,5 @@
 import { Entity } from '@tpluscode/fun-ddr'
+import urlSlug from 'url-slug'
 
 export { selectFactTableSource } from './createFactTable'
 export { unselectFactTable } from './unselectFactTable'
@@ -8,9 +9,12 @@ export interface Project extends Entity {
   name: string;
   archived: 'true' | 'false';
   factTableSource?: string;
+  baseUri: string;
 }
 
 export { createProject } from './createProject'
-export { renameProject } from './renameProject'
+export { updateProject } from './updateProject'
 export { createSource } from './createSource'
 export { archiveProject } from './archiveProject'
+
+export const temporaryUri = (name: string) => `http://cube.data/${urlSlug(name)}/`
