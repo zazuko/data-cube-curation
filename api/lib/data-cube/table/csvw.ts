5a4221b7760af944ae47a5ad89e50f8bd2997d79
tpluscode <tpluscode@users.noreply.github.com>
2020-01-13 12:16:28 +0100

    fix: mapping preview did not produce expected output

--- api/lib/data-cube/table/csvw.ts
+++ api/lib/data-cube/table/csvw.ts
@@ -7,12 +7,12 @@ import { NotFoundError } from '../../error'
 
 export const get = asyncMiddleware(async (req: express.Request, res: express.Response) => {
   const tableId = await getTableId(req.resourceId)
-  const dataset = await getTableAndSource(req.resourceId)
 
   if (!tableId) {
     throw new NotFoundError()
   }
 
+  const dataset = await getTableAndSource(tableId)
   const csvw = await buildCsvw({ dataset, tableId })
   res.graph(csvw._node.dataset)
 })
--- api/lib/read-graphs/table/csvw.ts
+++ api/lib/read-graphs/table/csvw.ts
@@ -3,21 +3,19 @@ import { describe } from '../../sparql'
 import { dataCube, schema, rdf } from '../../namespaces'
 import { getClient } from '../sparqlClient'
 
-export async function getTableAndSource (csvwResourceId: string) {
+export async function getTableAndSource (tableId: string) {
   // eslint-disable-next-line jest/valid-describe
-  return $rdf.dataset().import(await describe('?table', '?source', '?attribute', '?column', '?project', '?referencedTable')
+  return $rdf.dataset().import(await describe(tableId, '?source', '?attribute', '?column', '?project', '?referencedTable')
     .prefixes({ dataCube, schema, rdf })
     .where(`
-        <${csvwResourceId}> dataCube:table ?table .
-
-        ?tableId a dataCube:Table ;
+        <${tableId}> a dataCube:Table ;
                    dataCube:source ?source ;
                    dataCube:project ?project .
 
         ?source dataCube:column ?column .
 
         OPTIONAL {
-          ?attribute dataCube:table ?tableId .
+          ?attribute dataCube:table <${tableId}> .
         }
 
         OPTIONAL {
