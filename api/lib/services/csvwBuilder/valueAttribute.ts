2d1afb0e11eefff81082592a6c260894c2a200fc
tpluscode <tpluscode@users.noreply.github.com>
2020-01-22 17:40:53 +0100

    fix: map parameters to csvw (derived) datatype according to spec

--- api/lib/services/csvwBuilder/valueAttribute.ts
+++ api/lib/services/csvwBuilder/valueAttribute.ts
@@ -1,19 +1,63 @@
-import { ResourceIndexer } from '@tpluscode/rdfine'
+import { RdfResource } from '@tpluscode/rdfine'
 import { literal } from '@rdfjs/data-model'
+import { shrink } from '@zazuko/rdf-vocabularies'
 import * as Table from '../../read-model/Table/index'
 import * as Csvw from './index'
-import { csvw } from '../../namespaces'
+import { csvw, rdf } from '../../namespaces'
 
-export function valueAttributeToCsvwColumn (attribute: Table.ValueAttribute, csvwColumn: Csvw.Column & ResourceIndexer) {
-  attribute.parameters && Object.entries(attribute.parameters)
-    .forEach(([param, value]) => {
-      csvwColumn[csvw(param).value] = literal(value)
-    })
+const xsdPrefix = /^xsd:(.+)/
 
+function getBuiltInDatatypeName (datatype: RdfResource): string | null {
+  switch (datatype.id.value) {
+    case (csvw.JSON.value):
+      return 'json'
+    case (rdf.HTML.value):
+      return 'html'
+    case (rdf.XMLLiteral.value):
+      return 'xml'
+  }
+
+  const shrunk = shrink(datatype.id.value)
+  const matches = shrunk.match(xsdPrefix)
+
+  if (!matches) {
+    return null
+  }
+
+  switch (matches[1]) {
+    case 'double':
+      return 'number'
+    case 'base64Binary':
+      return 'binary'
+    case 'dateTime':
+      return 'datetime'
+    case 'anyAtomicType':
+      return 'any'
+  }
+
+  return matches[1]
+}
+
+export function valueAttributeToCsvwColumn (attribute: Table.ValueAttribute, csvwColumn: Csvw.Column) {
   if (attribute.language) {
     csvwColumn.language = attribute.language
   } else if (attribute.datatype) {
-    csvwColumn.datatype = attribute.datatype
+    const builtInType = getBuiltInDatatypeName(attribute.datatype)
+    const hasParameters = attribute.parameters && Object.entries(attribute.parameters).length > 0
+
+    if (builtInType && hasParameters) {
+      const derivedDatatype = csvwColumn._create(csvwColumn._node.blankNode())
+      derivedDatatype[csvw.base.value] = literal(builtInType)
+
+      Object.entries(attribute.parameters)
+        .forEach(([param, value]) => {
+          derivedDatatype[csvw(param).value] = literal(value)
+        })
+
+      csvwColumn.datatype = derivedDatatype
+    } else {
+      csvwColumn.datatype = attribute.datatype
+    }
   }
 
   return csvwColumn
