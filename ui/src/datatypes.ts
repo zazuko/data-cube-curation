da232840ae0428358c9dafde31333941ad6d83c0
Martin Maillard <self@martin-maillard.com>
2020-01-23 16:54:40 +0100

    refactor(ui): improve datatype definitions

--- ui/src/datatypes.ts
+++ ui/src/datatypes.ts
@@ -1,59 +1,63 @@
 import { expand } from '@zazuko/rdf-vocabularies'
-import { DataTypeOption, ResourceId, DataTypeParam } from '@/types'
+import { DataTypeOption, DataTypeOptionKind as Kind, ResourceId, DataTypeParam } from '@/types'
 
-const defaultParams: DataTypeParam[] = ['default']
+const defaultParams = ['default']
+const kindParams = {
+  [Kind.Any]: defaultParams,
+  [Kind.Numeric]: ['default', 'format'],
+  [Kind.Date]: ['default', 'format'],
+  [Kind.Duration]: ['default', 'format']
+}
 
 export const all: DataTypeOption[] = [
-  { name: 'any', uri: expand('xsd:anyAtomicType'), params: defaultParams },
-  { name: 'anyURI', uri: expand('xsd:anyURI'), params: defaultParams },
-  { name: 'binary', uri: expand('xsd:base64Binary'), params: defaultParams },
-  { name: 'boolean', uri: expand('xsd:boolean'), params: [...defaultParams, 'format'] },
-  { name: 'date', uri: expand('xsd:date'), params: [...defaultParams, 'format'] },
-  { name: 'datetime', uri: expand('xsd:dateTime'), params: [...defaultParams, 'format'] },
-  { name: 'dateTimeStamp', uri: expand('xsd:dateTimeStamp'), params: defaultParams },
-  { name: 'decimal', uri: expand('xsd:decimal'), params: [...defaultParams, 'format'] },
-  { name: 'integer', uri: expand('xsd:integer'), params: [...defaultParams, 'format'] },
-  { name: 'long', uri: expand('xsd:long'), params: [...defaultParams, 'format'] },
-  { name: 'int', uri: expand('xsd:int'), params: [...defaultParams, 'format'] },
-  { name: 'short', uri: expand('xsd:short'), params: [...defaultParams, 'format'] },
-  { name: 'byte', uri: expand('xsd:byte'), params: [...defaultParams, 'format'] },
-  { name: 'nonNegativeInteger', uri: expand('xsd:nonNegativeInteger'), params: [...defaultParams, 'format'] },
-  { name: 'positiveInteger', uri: expand('xsd:positiveInteger'), params: [...defaultParams, 'format'] },
-  { name: 'unsignedLong', uri: expand('xsd:unsignedLong'), params: [...defaultParams, 'format'] },
-  { name: 'unsignedInt', uri: expand('xsd:unsignedInt'), params: [...defaultParams, 'format'] },
-  { name: 'unsignedShort', uri: expand('xsd:unsignedShort'), params: [...defaultParams, 'format'] },
-  { name: 'unsignedByte', uri: expand('xsd:unsignedByte'), params: [...defaultParams, 'format'] },
-  { name: 'nonPositiveInteger', uri: expand('xsd:nonPositiveInteger'), params: [...defaultParams, 'format'] },
-  { name: 'negativeInteger', uri: expand('xsd:negativeInteger'), params: [...defaultParams, 'format'] },
-  { name: 'number', uri: expand('xsd:double'), params: [...defaultParams, 'format'] },
-  { name: 'duration', uri: expand('xsd:duration'), params: [...defaultParams, 'format'] },
-  { name: 'dayTimeDuration', uri: expand('xsd:dayTimeDuration'), params: [...defaultParams, 'format'] },
-  { name: 'yearMonthDuration', uri: expand('xsd:yearhMonthDuration'), params: [...defaultParams, 'format'] },
-  { name: 'float', uri: expand('xsd:float'), params: [...defaultParams, 'format'] },
-  { name: 'gDay', uri: expand('xsd:gDay'), params: [...defaultParams, 'format'] },
-  { name: 'gMonth', uri: expand('xsd:gMonth'), params: [...defaultParams, 'format'] },
-  { name: 'gMonthDay', uri: expand('xsd:gMonthDay'), params: [...defaultParams, 'format'] },
-  { name: 'gYear', uri: expand('xsd:gYear'), params: [...defaultParams, 'format'] },
-  { name: 'gYearMonth', uri: expand('xsd:gYearMonth'), params: [...defaultParams, 'format'] },
-  { name: 'hexBinary', uri: expand('xsd:hexBinary'), params: defaultParams },
-  { name: 'QName', uri: expand('xsd:QName'), params: defaultParams },
-  { name: 'string', uri: expand('xsd:string'), params: ['language', ...defaultParams] },
-  { name: 'normalizedString', uri: expand('xsd:normalizedString'), params: defaultParams },
-  { name: 'token', uri: expand('xsd:token'), params: defaultParams },
-  { name: 'language', uri: expand('xsd:language'), params: defaultParams },
-  { name: 'Name', uri: expand('xsd:Name'), params: defaultParams },
-  { name: 'NMTOKEN', uri: expand('xsd:NMTOKEN'), params: defaultParams },
-  { name: 'xml', uri: expand('rdf:xml'), params: defaultParams },
-  { name: 'html', uri: expand('rdf:html'), params: defaultParams },
-  { name: 'json', uri: expand('csvw:json'), params: defaultParams },
-  { name: 'time', uri: expand('xsd:time'), params: [...defaultParams, 'format'] }
-]
-
-const numericTypes = ['decimal', 'integer', 'long', 'int', 'short', 'byte', 'nonNegativeInteger', 'positiveInteger', 'unsignedLong', 'unsignedInt', 'unsignedShort', 'unsignedByte', 'nonPositiveInteger', 'negativeInteger', 'number', 'float']
-
-const dateTypes = ['date', 'datetime', 'dateTimeStamp', 'gDay', 'gMonth', 'gMonthDay', 'gYear', 'gYearMonth', 'time']
-
-const durationTypes = ['duration', 'dayTimeDuration', 'yearMonthDuration']
+  { name: 'any', uri: 'xsd:anyAtomicType', kind: Kind.Any },
+  { name: 'anyURI', uri: 'xsd:anyURI', kind: Kind.Any },
+  { name: 'binary', uri: 'xsd:base64Binary', kind: Kind.Any },
+  { name: 'boolean', uri: 'xsd:boolean', kind: Kind.Any, params: [...defaultParams, 'format'] },
+  { name: 'date', uri: 'xsd:date', kind: Kind.Date },
+  { name: 'datetime', uri: 'xsd:dateTime', kind: Kind.Date },
+  { name: 'dateTimeStamp', uri: 'xsd:dateTimeStamp', kind: Kind.Date },
+  { name: 'decimal', uri: 'xsd:decimal', kind: Kind.Numeric },
+  { name: 'integer', uri: 'xsd:integer', kind: Kind.Numeric },
+  { name: 'long', uri: 'xsd:long', kind: Kind.Numeric },
+  { name: 'int', uri: 'xsd:int', kind: Kind.Numeric },
+  { name: 'short', uri: 'xsd:short', kind: Kind.Numeric },
+  { name: 'byte', uri: 'xsd:byte', kind: Kind.Numeric },
+  { name: 'nonNegativeInteger', uri: 'xsd:nonNegativeInteger', kind: Kind.Numeric },
+  { name: 'positiveInteger', uri: 'xsd:positiveInteger', kind: Kind.Numeric },
+  { name: 'unsignedLong', uri: 'xsd:unsignedLong', kind: Kind.Numeric },
+  { name: 'unsignedInt', uri: 'xsd:unsignedInt', kind: Kind.Numeric },
+  { name: 'unsignedShort', uri: 'xsd:unsignedShort', kind: Kind.Numeric },
+  { name: 'unsignedByte', uri: 'xsd:unsignedByte', kind: Kind.Numeric },
+  { name: 'nonPositiveInteger', uri: 'xsd:nonPositiveInteger', kind: Kind.Numeric },
+  { name: 'negativeInteger', uri: 'xsd:negativeInteger', kind: Kind.Numeric },
+  { name: 'number', uri: 'xsd:double', kind: Kind.Numeric },
+  { name: 'duration', uri: 'xsd:duration', kind: Kind.Duration },
+  { name: 'dayTimeDuration', uri: 'xsd:dayTimeDuration', kind: Kind.Duration },
+  { name: 'yearMonthDuration', uri: 'xsd:yearhMonthDuration', kind: Kind.Duration },
+  { name: 'float', uri: 'xsd:float', kind: Kind.Numeric },
+  { name: 'gDay', uri: 'xsd:gDay', kind: Kind.Date },
+  { name: 'gMonth', uri: 'xsd:gMonth', kind: Kind.Date },
+  { name: 'gMonthDay', uri: 'xsd:gMonthDay', kind: Kind.Date },
+  { name: 'gYear', uri: 'xsd:gYear', kind: Kind.Date },
+  { name: 'gYearMonth', uri: 'xsd:gYearMonth', kind: Kind.Date },
+  { name: 'hexBinary', uri: 'xsd:hexBinary', kind: Kind.Any },
+  { name: 'QName', uri: 'xsd:QName', kind: Kind.Any },
+  { name: 'string', uri: 'xsd:string', kind: Kind.Any, params: ['language', ...defaultParams] },
+  { name: 'normalizedString', uri: 'xsd:normalizedString', kind: Kind.Any },
+  { name: 'token', uri: 'xsd:token', kind: Kind.Any },
+  { name: 'language', uri: 'xsd:language', kind: Kind.Any },
+  { name: 'Name', uri: 'xsd:Name', kind: Kind.Any },
+  { name: 'NMTOKEN', uri: 'xsd:NMTOKEN', kind: Kind.Any },
+  { name: 'xml', uri: 'rdf:xml', kind: Kind.Any },
+  { name: 'html', uri: 'rdf:html', kind: Kind.Any },
+  { name: 'json', uri: 'csvw:json', kind: Kind.Any },
+  { name: 'time', uri: 'xsd:time', kind: Kind.Date }
+].map((dt) => ({
+  ...dt,
+  uri: expand(dt.uri),
+  params: (dt.params ?? kindParams[dt.kind]) as DataTypeParam[]
+}))
 
 const nameIndex: Record<string, DataTypeOption> = all.reduce((acc, dt) => ({ ...acc, [dt.name]: dt }), {})
 const uriIndex: Record<ResourceId, DataTypeOption> = all.reduce((acc, dt) => ({ ...acc, [dt.uri]: dt }), {})
@@ -69,13 +73,13 @@ export function byURI (uri: ResourceId): DataTypeOption | null {
 export const defaultURI: ResourceId = expand('xsd:string')
 
 export function isNumericType (name: string): boolean {
-  return numericTypes.includes(name)
+  return byName(name)?.kind === Kind.Numeric
 }
 
 export function isDateType (name: string): boolean {
-  return dateTypes.includes(name)
+  return byName(name)?.kind === Kind.Date
 }
 
 export function isDurationType (name: string): boolean {
-  return durationTypes.includes(name)
+  return byName(name)?.kind === Kind.Duration
 }
