b5934926365b9ddc971c814e739bec84c7880bc7
tpluscode <tpluscode@users.noreply.github.com>
2020-04-07 16:06:27 +0200

    refactor: log SPARQL queries

--- api/lib/sparql/index.ts
+++ api/lib/sparql/index.ts
@@ -1,7 +1,7 @@
+/* eslint-disable import/export */
 import authHeader from './authentication'
-import { Term } from 'rdf-js'
+import { Quad, Term } from 'rdf-js'
 import rdf from 'rdf-ext'
-import { ParsingQuery } from 'sparql-http-client/ParsingQuery'
 import { getClient } from '../read-graphs/sparqlClient'
 import {
   SparqlAskExecutable,
@@ -11,41 +11,42 @@ import {
 } from '@tpluscode/sparql-builder/lib'
 import DatasetExt from 'rdf-ext/lib/Dataset'
 import env from '../env'
+import debug from 'debug'
 
-export function ask <T> (query: SparqlAskExecutable, client: ParsingQuery = getClient()): Promise<boolean> {
-  return query.execute(client, {
-    headers: {
-      authentication: authHeader,
-    },
-    base: env.BASE_URI,
-  })
+const log = debug('SPARQL:query')
+const sparqlOptions = {
+  headers: {
+    authentication: authHeader,
+  },
+  base: env.BASE_URI,
 }
 
-export function select <T> (query: SparqlQueryExecutable, client: ParsingQuery = getClient()): Promise<Record<string, Term>[]> {
-  return query.execute(client, {
-    headers: {
-      authentication: authHeader,
-    },
-    base: env.BASE_URI,
-  })
+export function execute (query: SparqlAskExecutable): Promise<boolean>
+export function execute (query: SparqlUpdateExecutable): Promise<void>
+export function execute (query: SparqlQueryExecutable): Promise<readonly Record<string, Term>[]>
+export function execute (query: SparqlGraphQueryExecutable): Promise<Quad[]>
+export function execute (query) {
+  if (log.enabled) {
+    log(query.build(sparqlOptions))
+  }
+
+  return query.execute(getClient(), sparqlOptions)
 }
 
-export async function construct <T> (query: SparqlGraphQueryExecutable, client: ParsingQuery = getClient()): Promise<DatasetExt> {
-  const quads = await query.execute(client, {
-    headers: {
-      authentication: authHeader,
-    },
-    base: env.BASE_URI,
-  })
+export function ask <T> (query: SparqlAskExecutable): Promise<boolean> {
+  return execute(query)
+}
+
+export function select <T> (query: SparqlQueryExecutable): Promise<readonly Record<string, Term>[]> {
+  return execute(query)
+}
+
+export async function construct <T> (query: SparqlGraphQueryExecutable): Promise<DatasetExt> {
+  const quads = await execute(query)
 
   return rdf.dataset(quads)
 }
 
-export function update <T> (query: SparqlUpdateExecutable, client: ParsingQuery = getClient()): Promise<void> {
-  return query.execute(client, {
-    headers: {
-      authentication: authHeader,
-    },
-    base: env.BASE_URI,
-  })
+export function update <T> (query: SparqlUpdateExecutable): Promise<void> {
+  return execute(query)
 }
