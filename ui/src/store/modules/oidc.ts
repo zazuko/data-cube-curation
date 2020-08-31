9316da5e23365b518102609f8a7371c076ccf06d
Quentin Gliech <quentingliech@gmail.com>
2020-08-31 11:15:57 +0200

    feat: make the oidc config runtime configurable
    This fixes issues when using Dex and Auth0 as IdPs.

--- ui/src/store/modules/oidc.ts
+++ ui/src/store/modules/oidc.ts
@@ -3,7 +3,6 @@ import { vuexOidcCreateStoreModule } from 'vuex-oidc'
 export const oidc = () => vuexOidcCreateStoreModule({
   redirectUri: window.location.origin + '/app/oidc-callback',
   responseType: 'code',
-  scope: 'profile pipelines:read pipelines:write',
   ...window.oidc,
 }, {
   routeBase: '/app/',
