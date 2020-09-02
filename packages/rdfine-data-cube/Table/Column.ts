0c00a73b0b8403d16a1005c4705571d01c4e990f
tpluscode <tpluscode@users.noreply.github.com>
2020-09-02 16:53:57 +0200

    refactor: update alcaeus and rdfine

--- packages/rdfine-data-cube/Table/Column.ts
+++ packages/rdfine-data-cube/Table/Column.ts
@@ -1,10 +1,11 @@
-import RdfResourceImpl, { Constructor, property } from '@tpluscode/rdfine'
+import RdfResourceImpl, { Constructor, namespace, property } from '@tpluscode/rdfine'
 import { schema, dtype } from '@tpluscode/rdf-ns-builders'
 import { ResourceNode } from '@tpluscode/rdfine/RdfResource'
 import * as Table from './index'
 import { dataCube } from '../namespaces'
 
 export function ColumnMixin<TBase extends Constructor> (Base: TBase) {
+  @namespace(dataCube)
   class Column extends Base implements Table.Column {
     @property.literal({ path: schema.name })
     public name!: string
