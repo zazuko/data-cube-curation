c54515540186ef61f90c33476285076f75ef5c78
tpluscode <tpluscode@users.noreply.github.com>
2020-10-09 13:53:53 +0200

    fix: ui should automatically renew auth token

--- ui/src/store/modules/oidc.ts
+++ ui/src/store/modules/oidc.ts
@@ -3,6 +3,7 @@ import { vuexOidcCreateStoreModule } from 'vuex-oidc'
 export const oidc = () => vuexOidcCreateStoreModule({
   redirectUri: window.location.origin + '/app/oidc-callback',
   responseType: 'code',
+  automaticSilentRenew: true,
   ...window.oidc,
 }, {
   routeBase: '/app/',
